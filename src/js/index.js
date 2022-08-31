0a8a27cdf374d6bca9287fdaea8ecaad80e314c8
Felipe Gomes <felipe.vgomes10.dev@gmail.com>
2022-08-31 18:13:51 -0300

    feat: add toast

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,10 @@
   const button = document.querySelector(".button");
   const textArea = document.querySelector(".textarea");
 
+  const toastContainer = document.querySelector(".toast");
+  const toastMessage = document.querySelector(".toast-message");
+  const toastButton = document.querySelector(".toast-button");
+
   if (textArea.value) button.removeAttribute("disabled");
   else button.setAttribute("disabled", "true");
 
@@ -19,10 +23,18 @@
       window.open(`view-json?q=${JSON.stringify(JSON.parse(textArea.value))}`);
     } catch (error) {
       event.target.innerText = "INVALID JSON!";
+
+      toastContainer.classList.add("error", "active");
+      toastMessage.innerText = error.message;
+
       console.log(error);
       setTimeout(() => {
         button.innerText = "VIEW YOUR JSON";
       }, 2000 /* two seconds */);
     }
+  });
+
+  toastButton.addEventListener("click", () => {
+    toastContainer.classList.remove("error", "active");
   });
 })();
